47c6c78faea4ca8d7a933462b5c67561739fc888
Madhankumar-arch <madhanmadhan21479@gmail.com>
2026-04-29 11:44:38 +0530

    Time: 1905 ms (5.02%), Space: 55.5 MB (66.95%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] arr) {
+        int n = arr.length;
+        for(int i = 0;i<arr.length;i++){
+            int count = 0;
+            for(int j = 0;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                }
+            }
+            if(count>n/2){
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+}
